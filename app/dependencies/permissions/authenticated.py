54fe5749880bd9adb1acc0cf8890c016c3c2e95d
jungminchae <minchae3618@gmail.com>
2024-10-04 23:41:28 +0900

    fix is mysel permission by using allowed user

--- app/dependencies/permissions/authenticated.py
+++ app/dependencies/permissions/authenticated.py
@@ -1,18 +1,20 @@
 from typing import TYPE_CHECKING
 from fastapi import status
 from fastapi_mctools.exceptions import HTTPException
+from app.db.async_session import DB
+from app.orms.users import user_orm
 from app.dependencies.authentication import auth_dependency
 
 if TYPE_CHECKING:
     from app.models.users import User
 
 
-async def is_user_myself(user: auth_dependency.CurrentUser, user_id: int) -> "User":
+async def is_user_myself(db: DB, user: auth_dependency.CurrentUser, user_id: int) -> "User":
     if user.id != user_id and not user.is_superuser:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="권한이 없습니다.",
             code="FORBIDDEN",
         )
-
-    return user
+    allowed_user = await user_orm.get_by_id(db, user_id)
+    return allowed_user
